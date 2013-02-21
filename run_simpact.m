d75c1e9a9c617d81e290da32c3a085442f921ed9
Lucio Tolentino <seanluciotolentino@gmail.com>
2013-02-21 15:51:32 +0200

    added the stuff from startup to this file

--- run_simpact.m
+++ run_simpact.m
@@ -1,5 +1,28 @@
 addpath( [fileparts(which(mfilename)) '\lib'] );
-startup
+%STARTUP
+
+% Copyright 2009-2010 by Hummeling Engineering (www.hummeling.com) and
+% Lucio
+
+fprintf(1, ' ******* %s %s *******\n', datestr(now), mfilename)
+
+
+% ******* Settings *******
+format compact
+format short g
+warning('off', 'MATLAB:dispatcher:InexactCaseMatch')
+warning('off', 'MATLAB:dispatcher:InexactMatch')
+warning('off', 'MATLAB:dispatcher:pathWarning')
+warning('off', 'MATLAB:classChanged')
+warning('off', 'Simulink:SL_MATLABFcnIncompleteSimState')
+
+system_dependent('DirChangeHandleWarn', 'Never');
+
+
+% ******* Launch Project Menu *******
+if exist('jproject.m', 'file') == 2
+    jproject
+end
 
 %have to add it twice because somewhere startup removes the path
 addpath( [fileparts(which(mfilename)) '\lib'] );
